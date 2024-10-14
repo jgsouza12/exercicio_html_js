c9fdd75154e096f78aeca78c072b238004b97c3a
jgsouza12 <142275277+jgsouza12@users.noreply.github.com>
2024-10-13 22:23:53 -0300

    added files

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const campoA = parseFloat(document.getElementById('campoA').value);
+    const campoB = parseFloat(document.getElementById('campoB').value);
+
+    const messageDiv = document.getElementById('message');
+
+    if (campoB > campoA) {
+        messageDiv.textContent = 'O formulário é válido! Campo B é maior que Campo A.';
+        messageDiv.className = 'message success';
+    } else {
+        messageDiv.textContent = 'O formulário é inválido! Campo B deve ser maior que Campo A.';
+        messageDiv.className = 'message error';
+    }
+});
